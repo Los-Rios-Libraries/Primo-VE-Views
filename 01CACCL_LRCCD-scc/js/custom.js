8b1fe9141c3dec2dc9457ead69b5cc63f0b47b0e
jkarlsen <karlsej@scc.losrios.edu>
2019-11-12 15:26:04 -0800

    Enclose script adjusting positioning of footer in IIFE; also cache reference to scrollbar element

--- 01CACCL_LRCCD-scc/js/custom.js
+++ 01CACCL_LRCCD-scc/js/custom.js
@@ -25,37 +25,40 @@
 
 		templateUrl: custPackagePath + '/html/footer.html'
 	});
+	(function () {
+			// Footer (from NLNZ) - measure page once "is sticky" is put in and (try) to put footer after results. Posted on Primo listserv by Bond University, November 2019
 
-	// Footer (from NLNZ) - measure page once "is sticky" is put in and (try) to put footer after results. Posted on Primo listserv by Bond University, November 2019
+			// Instantiate variables that will be reset repeatedly in the listener function
+			var max = 0;
+			var winHeight = 0;
+			var scrollTop = 0;
+			var foot = 0;
+			// and let's have a small buffer before the footer
+			var buffer = 50;
+			window.addEventListener('scroll', function () {
 
-	// Instantiate variables that will be reset repeatedly in the listener function
-	var max = 0;
-	var winHeight = 0;
-	var scrollTop = 0;
-	var foot = 0;
-	// and let's have a small buffer before the footer
-	var buffer = 50;
-	window.addEventListener('scroll', function () {
+				// Total length of document
+				max = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight,
+					document.body.offsetHeight, document.documentElement.offsetHeight,
+					document.body.clientHeight, document.documentElement.clientHeight);
+				// Height of window
+				winHeight = window.innerHeight || (document.documentElement || document.body).clientHeight;
+				// Point of the top of the document visible on screen
+				scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+				// Height of footer
+				foot = Math.round(parseFloat(window.getComputedStyle(document.getElementById('footer')).height));
+				// check where we are in terms of scrolling and the footer
+				var stuckWin = document.querySelectorAll('.primo-scrollbar.is-stuck')[0];
+				// check for undefined to avoid TypeErrors
+				if (stuckWin) {
+					if (scrollTop + winHeight >= max - foot) {
+						stuckWin.style.maxHeight = 'calc(100% - ' + Math.abs(max - winHeight - scrollTop - foot - buffer) + 'px)';
+					} else {
+						stuckWin.style.maxHeight = 'calc(100% - 2em)';
+					}
+				}
 
-		// Total length of document
-		max = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight,
-			document.body.offsetHeight, document.documentElement.offsetHeight,
-			document.body.clientHeight, document.documentElement.clientHeight);
-		// Height of window
-		winHeight = window.innerHeight || (document.documentElement || document.body).clientHeight;
-		// Point of the top of the document visible on screen
-		scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-		// Height of footer
-		foot = Math.round(parseFloat(window.getComputedStyle(document.getElementById('footer')).height));
-		// check where we are in terms of scrolling and the footer
-		if (scrollTop + winHeight >= max - foot) {
-			document.querySelectorAll('.primo-scrollbar.is-stuck')[0].style.maxHeight = 'calc(100% - ' + Math.abs(max - winHeight - scrollTop - foot - buffer) + 'px)';
-		} else {
-			document.querySelectorAll('.primo-scrollbar.is-stuck')[0].style.maxHeight = 'calc(100% - 2em)';
-		}
-	});
+			});
 
-
-
-
+		}());
 }());
