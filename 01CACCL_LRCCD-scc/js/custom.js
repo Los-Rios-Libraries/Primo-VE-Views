584ab9871e98f3a1325a9b9eebc1f17ba1c12269
jkarlsen <karlsej@scc.losrios.edu>
2021-01-06 10:39:15 -0800

    Remove message about lack of requesting

--- 01CACCL_LRCCD-scc/js/custom.js
+++ 01CACCL_LRCCD-scc/js/custom.js
@@ -574,10 +574,6 @@
 			};
 		}
 	});
-	// note on lack of requesting during COVID-19 closure
-	app.component('prmOpacAfter', { 
-		template: '<md-content layout-margin><p><md-icon md-svg-icon="action:ic_announcement_24px"></md-icon>Please note: during the temporary library closures in response to the COVID-19 pandemic, item requests are unavailable. We apologize for this inconvenience and encourage you to explore online library resources.</p></md-content>'
-	});
 	app.component('prmFinesAfter', { // show message explaining how to pay fines
 		bindings: {
 			parentCtrl: '<'
