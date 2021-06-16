695918bbc7c1f80318c410cfc79d7ad8bda443bb
jkarlsen <karlsej@scc.losrios.edu>
2021-06-16 16:06:11 -0700

    fix typo in ng-href

--- 01CACCL_LRCCD-default/js/custom.js
+++ 01CACCL_LRCCD-default/js/custom.js
@@ -7,7 +7,7 @@
         });
     app.component('lrViewButtons', {
         bindings: {parentCtrl: '<'},
-		template: '<div layout="row" layout-align="center" style="font-weight:bold;"><div layout="column" layout-align="center"><p>See this record in a college-level view:</p></div><div layout="column"><ul layout="row"><li ng-repeat="view in $ctrl.views track by $index" style="list-style-type:none;"><md-button ng-href="https://caccl-lrccd.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma991001986914805325&amp;vid=01CACCL_LRCCD:{{view}}" target="_blank">{{view}}</md-button></li></ul></div></div>',
+		template: '<div layout="row" layout-align="center" style="font-weight:bold;"><div layout="column" layout-align="center"><p>See this record in a college-level view:</p></div><div layout="column"><ul layout="row"><li ng-repeat="view in $ctrl.views track by $index" style="list-style-type:none;"><md-button ng-href="https://caccl-lrccd.primo.exlibrisgroup.com/discovery/fulldisplay?docid={{$ctrl.docID}}&amp;vid=01CACCL_LRCCD:{{view}}" target="_blank">{{view}}</md-button></li></ul></div></div>',
         controller: function() {
             var vm = this;
             vm.views = ['arc', 'crc', 'flc', 'scc'];
