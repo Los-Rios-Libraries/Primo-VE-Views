4a23297636134c7c204a0a49be84331977fe4da8
jkarlsen <karlsej@scc.losrios.edu>
2021-09-02 13:36:01 -0700

    Add search scope to URL for alternate views from default
    Needed for unavailable to be properly suppressed.

--- 01CACCL_LRCCD-default/js/custom.js
+++ 01CACCL_LRCCD-default/js/custom.js
@@ -7,7 +7,7 @@
         });
     app.component('lrViewButtons', {
         bindings: {parentCtrl: '<'},
-		template: '<div layout="row" layout-align="center" style="font-weight:bold;" ng-if="$ctrl.fullView();"><div layout="column" layout-align="center"><p>See this record in a college-level OneSearch view:</p></div><div layout="column"><ul layout="row"><li ng-repeat="view in $ctrl.views track by $index" style="list-style-type:none;"><md-button ng-href="https://caccl-lrccd.primo.exlibrisgroup.com/discovery/fulldisplay?docid={{$ctrl.docID}}&amp;vid=01CACCL_LRCCD:{{view}}" target="_blank">{{view}}</md-button></li></ul></div></div>',
+		template: '<div layout="row" layout-align="center" style="font-weight:bold;" ng-if="$ctrl.fullView();"><div layout="column" layout-align="center"><p>See this record in a college-level OneSearch view:</p></div><div layout="column"><ul layout="row"><li ng-repeat="view in $ctrl.views track by $index" style="list-style-type:none;"><md-button ng-href="https://caccl-lrccd.primo.exlibrisgroup.com/discovery/fulldisplay?docid={{$ctrl.docID}}&amp;vid=01CACCL_LRCCD:{{view}}&amp;search_scope={{view}}_everything" target="_blank">{{view}}</md-button></li></ul></div></div>',
         controller: function() {
             var vm = this;
             vm.views = ['arc', 'crc', 'flc', 'scc'];
