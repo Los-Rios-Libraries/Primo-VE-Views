2e559b1c01e6aacecf37325c1a28d03bfc54cb5d
jkarlsen <karlsej@scc.losrios.edu>
2022-11-07 13:39:15 -0800

    Update controller in default.js for AngularJS 1.8

--- 01CACCL_LRCCD-default/js/custom.js
+++ 01CACCL_LRCCD-default/js/custom.js
@@ -8,16 +8,19 @@
     app.component('lrViewButtons', {
         bindings: {parentCtrl: '<'},
 		template: '<div layout="row" layout-align="center" style="font-weight:bold;" ng-if="$ctrl.fullView();"><div layout="column" layout-align="center"><p>See this record in a college-level OneSearch view:</p></div><div layout="column"><ul layout="row"><li ng-repeat="view in $ctrl.views track by $index" style="list-style-type:none;"><md-button ng-href="https://caccl-lrccd.primo.exlibrisgroup.com/discovery/fulldisplay?docid={{$ctrl.docID}}&amp;vid=01CACCL_LRCCD:{{view}}&amp;search_scope={{view}}_everything" target="_blank">{{view}}</md-button></li></ul></div></div>',
-        controller: function() {
-            var vm = this;
-            vm.views = ['arc', 'crc', 'flc', 'scc'];
-            vm.docID = vm.parentCtrl.$stateParams.docid;
-            vm.fullView = function() {
-                if ((vm.parentCtrl.isFullView === true) && (typeof(vm.parentCtrl.isOverlayFullView) === 'undefined')) {
-                    return true;
-                 }  
-            };
-        }
+	controller: function () {
+		var vm = this;
+		vm.$onInit = function () {
+			vm.views = ['arc', 'crc', 'flc', 'scc'];
+			vm.docID = vm.parentCtrl.$stateParams.docid;
+			vm.fullView = function () {
+				if ((vm.parentCtrl.isFullView === true) && (typeof (vm.parentCtrl.isOverlayFullView) === 'undefined')) {
+					return true;
+				}
+			};
+		};
+
+	}
     });
 }());
 
