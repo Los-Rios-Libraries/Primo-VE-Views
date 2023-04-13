d77b935caf749ad74b5691e2c48c67671e8a3613
jkarlsen <karlsej@scc.losrios.edu>
2023-04-13 14:45:15 -0700

    Exclude chat widget from Alma-D viewer pages

--- 01CACCL_LRCCD-scc/js/custom.js
+++ 01CACCL_LRCCD-scc/js/custom.js
@@ -1074,13 +1074,16 @@
 
 		}());
 	(function () { // load libchat
+		var almaDStr = 'https://caccl-lrccd.primo.exlibrisgroup.com/discovery/delivery/01CACCL_LRCCD';
 		var div = document.createElement('div');
 		div.id = 'libchat_' + libchatHash;
 		document.getElementsByTagName('body')[0].appendChild(div);
 		var scr = document.createElement('script');
 		scr.src = 'https://answers.library.losrios.edu/load_chat.php?hash=' + libchatHash;
 		setTimeout(function () {
-			document.getElementsByTagName('body')[0].appendChild(scr);
+			if (location.href.indexOf(almaDStr) !== 0) { // don't include in Alma viewer
+				document.getElementsByTagName('body')[0].appendChild(scr);
+			}
 		}, 2000);
 	}());
 }());
