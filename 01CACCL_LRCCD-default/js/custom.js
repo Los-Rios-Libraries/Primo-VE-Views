092bb46d719d932888234bf6f07d03102c896dbf
jkarlsen <karlsej@scc.losrios.edu>
2023-07-20 16:11:02 -0700

    Update lrcreator component in default view

--- 01CACCL_LRCCD-default/js/custom.js
+++ 01CACCL_LRCCD-default/js/custom.js
@@ -80,27 +80,31 @@
 		},
 		templateUrl: custPackagePath + '/html/local-creator.html',
 		controller: function () {
-			var vm = this;
-			vm.$onInit = function() {
-				var lrCrField = vm.parentCtrl.item.pnx.display.lds09; // this is always an array and will normally have one member but there could be more
+			const vm = this;
+			vm.$onInit = () => {
+				const lrCrField = vm.parentCtrl.item.pnx.display.lds09; // this is always an array and will normally have one member but there could be more
 				vm.creators = []; // we push creator objects into an array that the template iterates through
 				if (lrCrField) {
-					for (var i = 0; i < lrCrField.length; i++) { // for each array member we will split by delimiter and process
-						var data = JSON.parse(lrCrField[i]);
+					for (let field of lrCrField) { // for each array member we will split by delimiter and process
+						const data = JSON.parse(field);
 						vm.creatorType = 'creator'; // default if role is not defined
-						if ((data.role) && (data.role !== 'z')) { // get role
+						if (data.role) { // get role
 							vm.creatorType = data.role;
 						}
-						var creator = {};
+						const creator = {};
 						if ((((data.crName) && (data.crName !== 'z')) && (data.currency) && (data.currency !== 'z'))) {
 							vm.badge = true; // set property that allows the badges to show
 							creator.crName = data.crName;
-							var zTitle = 'affiliated';
-							var connector = 'with';
-							var currency = 'is';
-							var art = '';
-							var title = zTitle;
-							if ((data.position) && (data.position !== 'z')) {
+							const zTitle = 'affiliated';
+							let connector = 'with';
+							let currency = 'is';
+							let deceased = false;
+							if ((data.deceased) && (data.deceased === 'deceased')) {
+								deceased = true;
+							}
+							let art = '';
+							let title = zTitle;
+							if (data.position) {
 								title = data.position;
 								connector = 'at';
 								if (/^(Interim )?President/.test(title)) {
@@ -117,29 +121,35 @@
 								}
 								if (data.currency === 'former') {
 									currency = 'is a former';
+									if (deceased === true) {
+										currency = 'was ' + art;
+									}
 									art = '';
 									if (title === zTitle) {
 										currency = 'was formerly';
+										if (deceased === true) {
+											currency = 'was';
+										}
 									}
 								}
 							}
-							creator.title = currency + ' ' + art + ' ' + title + ' ' + connector;
-							var college = 'Los Rios Community College District';
-							var crCol = [];
-							var collegePhr = '';
-							if ((data.college) && (data.college !== 'z')) {
-								for (var j = 0; j < libraries.length; j++) { // use libraries array defined at top of this file
-									if (data.college.indexOf(libraries[j].abbr) > -1) {
-										crCol.push(libraries[j].name + ' College');
+							creator.title = `${currency} ${art} ${title} ${connector}`;
+							let college = 'Los Rios Community College District';
+							const crCol = [];
+							let collegePhr = '';
+							if (data.college) {
+								for (let lib of libraries) { // use libraries array defined at top of this file
+									if (data.college.indexOf(lib.abbr) > -1) {
+										crCol.push(`${lib.name} College`);
 									}
 								}
-								var colNum = crCol.length; // vary syntax based on how many colleges are listed
+								let colNum = crCol.length; // vary syntax based on how many colleges are listed
 								if (colNum === 1) { // just one college
 								collegePhr = crCol[0];
 								} else if (colNum === 2) { // two colleges
-									collegePhr = crCol[0] + ' and ' + crCol[1];
+									collegePhr = `${crCol[0]} and ${crCol[1]}`;
 								} else { // moret than two
-									var lastMember = crCol[colNum - 1];
+									const lastMember = crCol[colNum - 1];
 									crCol.splice(colNum - 1, 1, 'and ' + lastMember);
 									collegePhr = crCol.join(', ');
 								}
@@ -147,8 +157,8 @@
 							}
 							creator.college = college;
 						}
-						var url = '';
-						if ((data.url) && (data.url !== 'z')) {
+						let url = '';
+						if (data.url) {
 							url = data.url;
 						}
 						creator.url = url;
